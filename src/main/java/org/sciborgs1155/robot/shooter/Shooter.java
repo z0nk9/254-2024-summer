17ef6918fa85620901ba3e214068bcd13bbd2596
Sunay Jain <sunay1j@gmail.com>
2025-07-29 12:10:09 -0400

    shooter done YAY only 4 more subsytems the commands and then unit tests (ik its a bad order but iajerhgaiuerhgiuyahr)

--- src/main/java/org/sciborgs1155/robot/shooter/Shooter.java
+++ src/main/java/org/sciborgs1155/robot/shooter/Shooter.java
@@ -1,5 +1,39 @@
 package org.sciborgs1155.robot.shooter;
 
+import org.sciborgs1155.robot.Robot;
+import static org.sciborgs1155.robot.shooter.ShooterConstants.kD;
+import static org.sciborgs1155.robot.shooter.ShooterConstants.kI;
+import static org.sciborgs1155.robot.shooter.ShooterConstants.kP;
+
+import edu.wpi.first.math.controller.PIDController;
+import static edu.wpi.first.units.Units.RadiansPerSecond;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class Shooter extends SubsystemBase {}
+public class Shooter extends SubsystemBase {
+  private final ShooterIO hardware;
+  private final PIDController pid;
+
+  public Shooter(ShooterIO hardware) {
+    this.hardware = hardware;
+    this.pid = new PIDController(kP, kI, kD);
+  }
+
+  public static Shooter create() {
+    return Robot.isReal() ? new Shooter(new RealShooter()) : new Shooter(new SimShooter());
+  }
+
+  public static Shooter none() {
+    return new Shooter(new NoShooter());
+  }
+
+  public Command shootAtSpeed(AngularVelocity speed) {
+    return run(
+        () -> {
+          hardware.setVoltage(
+              pid.calculate(
+                  hardware.getVelocity().in(RadiansPerSecond), speed.in(RadiansPerSecond)));
+        });
+  }
+}
--- src/main/java/org/sciborgs1155/robot/shooter/ShooterConstants.java
+++ src/main/java/org/sciborgs1155/robot/shooter/ShooterConstants.java
@@ -10,4 +10,9 @@ public class ShooterConstants {
   public static final double MOI = 0.001;
   public static final double GEARING = 32 / 18;
   public static final DCMotor GEARBOX = DCMotor.getKrakenX60(2);
+  public static final double kP = 1;
+  public static final double kI = 0;
+  public static final double kD = .1;
+  public static final double MAX_VELOCITY = 10;
+  public static final double MAX_ACCEL = 10;
 }
